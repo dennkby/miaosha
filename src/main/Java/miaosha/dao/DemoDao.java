37f001124e7bca88d673e6fab2cedfca04c6f459
dennkby <fyksmail@163.com>
2018-04-08 18:46:51 +0800

    new

---
+++ src/main/Java/miaosha/dao/DemoDao.java
@@ -0,0 +1,7 @@
+package miaosha.dao;
+
+/**
+ * Created by Tony on 2018/4/8.
+ */
+public class DemoDao {
+}
---
+++ src/main/Java/miaosha/service/DemoService.java
@@ -0,0 +1,7 @@
+package miaosha.service;
+
+/**
+ * Created by Tony on 2018/4/8.
+ */
+public class DemoService {
+}
