680f33d7ac228765d74a6cf94ec6b1e68c0af4d5
dennkby <fyksmail@163.com>
2018-04-08 17:14:56 +0800

    c

---
+++ src/main/Java/miaosha/MainApplication.java
@@ -0,0 +1,15 @@
+package miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Tony on 2018/4/8.
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
